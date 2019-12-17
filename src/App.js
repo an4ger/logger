c55960097009a3598d26b2d07c982e76a3a42b2a
an4ger <an4ger@gmail.com>
2019-12-17 23:16:45 +0200

    store, reducers, modals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, {useEffect, Fragment} from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
+import './App.css'
 import M from 'materialize-css/dist/js/materialize.min'
-
 import SearchBar from './components/layout/SearchBar'
 import Logs from './components/logs/Logs'
+import AddBtn from './components/layout/AddBtn'
+import AddLogModal from './components/logs/AddLogModal'
+import EditLogModal from './components/logs/EditLogModal'
+import AddTechModal from './components/techs/AddTechModal'
+import TechListModal from './components/techs/TechListModal'
 
-import './App.css'
+import {Provider} from 'react-redux'
+import store from './store'
 
 const App = () => {
-  useEffect(() => {
-    // init materialize
-    M.AutoInit()
-  }, [])
+    useEffect(() => {
+        // init materialize
+        M.AutoInit()
+    }, [])
 
-  return (
-    <Fragment>
-      <SearchBar/>
-      <div className="container">
-        <Logs/>
-      </div>
-    </Fragment>
+    return (
+    <Provider store={store}>
+        <Fragment>
+            <SearchBar/>
+            <div className="container">
+                <AddBtn/>
+                <AddLogModal/>
+                <EditLogModal/>
+                <AddTechModal/>
+                <TechListModal/>
+                <Logs/>
+            </div>
+        </Fragment>
+    </Provider>
   )
 }
 
